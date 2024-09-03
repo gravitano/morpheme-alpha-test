f8a77a773b2681b4a05df3110370687afa2a91f1
Warsono <warsono16694@gmail.com>
2024-09-03 14:04:28 +0700

    chore: use alpha 7

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <Welcome />
+  <VAppBar shadow>
+    <VContainer centered>
+      <span class="font-semibold">Morpheme UI</span>
+    </VContainer>
+  </VAppBar>
+  <VContainer centered class="py-6">
+    <VTooltip placement="top">
+      <template #activator>
+        <VBtn> Hove me </VBtn>
+      </template>
+      <span>Hello :)</span>
+    </VTooltip>
+  </VContainer>
 </template>
